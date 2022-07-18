5aa462bc244fb66b9168f29239ea77e85c1b6844
Michal Wadowski <michal.wadowski@healthwareinternational.com>
2022-07-18 16:55:49 +0200

    cards Test Bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cardPrj';
+  posts = [
+    {
+      title: 'Neat Tree',
+      user: '@user',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'Content',
+    },
+    {
+      title: 'Mountain',
+      user: '@Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Content',
+    },
+    {
+      title: 'Biking',
+      user: '@Michau',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'Content',
+    },
+    {
+      title: 'No foto',
+      user: '@Michau',
+      imageUrl: '', // default image charged if no image url
+      content: 'Content',
+    },
+  ];
 }
